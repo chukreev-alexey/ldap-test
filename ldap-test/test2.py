cb6dc48955ceabc9cf74948277e9de60c2f402ff
Chukreyev Alexey <chukreyev.alexey@gmail.com>
2012-11-04 22:01:59 +0600

    LDAP Object Mapper was chosen - custom ldapom

--- ldap-test/test2.py
+++ ldap-test/test2.py
@@ -2,7 +2,7 @@
 
 # python-ldap library test
 
-from .libs import ldapom
+from utils import ldapom
 
 LDAP_HOST = '195.189.111.9'
 LDAP_PORT = 4389
@@ -14,12 +14,18 @@ server = 'ldap://%s:%d' % (LDAP_HOST, LDAP_PORT)
 
 
 
-lc = ldapom.LdapConnection(uri=server, base='dc=chukreev,dc=com', login=LDAP_USERNAME, password=LDAP_PASSWORD) # doctest: +SKIP
+lc = ldapom.LdapConnection(uri=server, base='dc=chukreev,dc=com',
+                           login=LDAP_USERNAME, password=LDAP_PASSWORD)
+
+## Get full schema from DN specified in `base`
+result = lc.query()
+
+## Select single node
 node = lc.get_ldap_node('uid=lehamaaan,ou=people,dc=chukreev,dc=com')
 #print node
 #print node.sn
 
 #node.sn = u'Чукреев Алексей' # change givenname
 #node.save() # save all change
-#print node sn # Unicode BUG
+#print node sn # Unicode BUG FIXME
 #node.delete() # delete
